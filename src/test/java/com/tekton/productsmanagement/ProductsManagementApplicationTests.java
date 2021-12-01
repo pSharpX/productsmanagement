dd708487ab783ad44c7e89a2e64e890bec1d6d29
psharpx <crivera2093@gmail.com>
2021-11-30 23:08:10 -0500

    initial commit

---
+++ src/test/java/com/tekton/productsmanagement/ProductsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekton.productsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
