70ccf889946bab13fdac874a0af25a8a6dbf6928
psharpx <crivera2093@gmail.com>
2021-12-05 15:06:08 -0500

    logging aspects definition

---
+++ src/main/java/com/tekton/productsmanagement/aop/LoggingAspect.java
@@ -0,0 +1,78 @@
+package com.tekton.productsmanagement.aop;
+
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+@Slf4j
+public class LoggingAspect {
+
+	@Pointcut(
+			"within(com.tekton.productsmanagement.catalog.repository..*) || within(com.tekton.productsmanagement.catalog.service..*) || within(com.tekton.productsmanagement.catalog.resource..*)"
+	)
+	public void monitor() { }
+
+	@AfterThrowing(pointcut = "monitor()", throwing = "e")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable e){
+		Object cause = Optional.ofNullable((Object) e.getCause()).orElse("NULL");
+
+		log.error("Exception in {}.{}() with cause = '{}' and exception = '{}'",
+				joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), cause,
+				e.getMessage(), e);
+	}
+
+	@Around("monitor()")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		LocalTime begin = null;
+		Object result = null;
+		final Signature signature = joinPoint.getSignature();
+
+		if(log.isDebugEnabled()) {
+			begin = LocalTime.now();
+		}
+
+		try {
+			result = joinPoint.proceed();
+			return result;
+		} catch (IllegalArgumentException e) {
+			log.error("Illegal argument: {} in {}#{}", objectToString(joinPoint.getArgs(), "NO ARGS"),
+					signature.getDeclaringTypeName(), signature.getName());
+			throw e;
+		} finally {
+			if(log.isDebugEnabled()){
+				final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+				LocalTime end = LocalTime.now();
+				LocalTime start = Optional.ofNullable(begin).orElse(end);
+				long duration = ChronoUnit.MILLIS.between(start, end);
+
+				String args = objectToString(joinPoint.getArgs(), "NO ARGS");
+				String answer = objectToString(result, "NO RESULT");
+
+				log.debug("Execute {}#{} start={} end={} duration={} ms. \nwith argument(s)={} \nresult={}",
+						signature.getDeclaringTypeName(), signature.getName(), start.format(formatter),
+						end.format(formatter), duration, args, answer);
+			}
+		}
+	}
+
+	private String objectToString(Object object, String otherwise) {
+		return Optional.ofNullable(object).map(o -> new ReflectionToStringBuilder(object,
+				RecursiveToStringStyle.JSON_STYLE).toString()).orElse(otherwise);
+	}
+
+}
---
+++ src/main/java/com/tekton/productsmanagement/config/LoggingAspectConfiguration.java
@@ -0,0 +1,18 @@
+package com.tekton.productsmanagement.config;
+
+import com.tekton.productsmanagement.aop.LoggingAspect;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+	@Bean
+	public LoggingAspect loggingAspect() {
+		return new LoggingAspect();
+	}
+
+}
