5226298c9b60c577cfab5d8bfc6fd391f8d2a92b
Christian Rivera <crivera2093@gmail.com>
2024-01-17 23:59:23 -0500

    feat: removed unused files and rename properties and image name

--- build_image.sh
+++ build_image.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#echo Building psharpx/productsmanagement-api:v1.0
-echo Building psharpx/productsmanagement-api:v1.0
+#echo Building psharpx/productsmanagement:v1.0
+echo Building psharpx/productsmanagement:v1.0
 
-docker build --no-cache -t psharpx/productsmanagement-api:v1.0 . -f Dockerfile
-#docker build --no-cache -t psharpx/productsmanagement-api:v1.0 . -f Dockerfile.client
+docker build --no-cache -t psharpx/productsmanagement:v1.0 . -f Dockerfile
+#docker build --no-cache -t psharpx/productsmanagement:v1.0 . -f Dockerfile.client
 
 echo Building was successful !!...
 
